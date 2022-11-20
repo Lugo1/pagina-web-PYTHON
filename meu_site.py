9ec0a3a4eef979d2492c58d8ad460d91429fc446
LugoRIO <lugon479@gmail.com>
2022-11-19 21:54:32 -0300

    deploy inicial

--- meu_site.py
+++ meu_site.py
@@ -20,7 +20,8 @@ def homepage():
 
 @app.route("/greet", methods=["POST", "GET"])
 def nombres():
-    flash("Hola! " + str(request.form['name_input'])+"!" + ", Gracias por comentar!")
+    flash("Waoo!: " + str(request.form['name_input']))
+    flash("Gracias por comentar!!!"+"Pareces inteligente! :)")
     return render_template("homepage.html")
     #return render_template("nombres.html")
     #return "<p>1. Juan...</p><p>2. Emi... </p><p>3. Jona </p>EMAIL: @@@"
